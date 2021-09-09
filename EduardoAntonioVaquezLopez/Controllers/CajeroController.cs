371ed7b1d4c5e9c11bfde1ae0ba397e3db36d0f3
EduardoVasquez21 <78975314+EduardoVasquez21@users.noreply.github.com>
2021-09-09 12:14:04 -0600

    Parcial1

---
+++ EduardoAntonioVaquezLopez/Controllers/CajeroController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EduardoAntonioVaquezLopez.Controllers
+{
+    public class CajeroController : Controller
+    {
+        // GET: Cajero
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Log(String monto)
+        { 
+            if (monto.Equals("5") && monto.Equals("10"))
+            {
+                return Redirect("/Cajero/Error");
+            }
+            return Redirect("/Cajero/SecundarioMenu");
+            //else
+            //{
+            //    return Redirect("/Cajero/Error");
+            //}
+            ////return Redirect("/Cajero/Error");
+
+        }
+
+        public ActionResult Error()
+        {
+
+            return View();
+        }
+
+        public ActionResult SecundarioMenu()
+        {
+
+            return View();
+        }
+
+    }
+}
---
+++ EduardoAntonioVaquezLopez/Models/ViewModels/CajeroViewModels.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace EduardoAntonioVaquezLopez.Models.ViewModels
+{
+    public class CajeroViewModels
+    {
+        [Display(Name = "Monto")]
+        [Required(ErrorMessage = "Campo requerido")]
+        public string monto { get; set; }
+    }
+}
